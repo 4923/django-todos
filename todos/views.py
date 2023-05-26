c4a5c8afbf7142f0225b29fb9d9b5d76ad736230
4923 <4923.py@gmail.com>
2023-05-26 09:56:44 +0900

    Create: todos 리스트 조회 및 상태변경을 위한 함수 생성
    referenced: https://glory-summer.tistory.com/194

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Todo 
+from .forms import TodoForm
 
 # Create your views here.
+@login_required 
+def index(request):
+    completed = request.GET.get('completed') or False
+    todos = Todo.objects.filter(completed=completed)
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.author = request.user # 작성자 정보 
+            todo.save()
+    else:
+        form = TodoForm()
+    
+    context = {
+        'form': form,
+        'todos': todos,
+    }
+    return render(request, 'todos/index.html', context)
+
+
+def complete(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.completed = True
+    todo.save()
+    return redirect('todos:index')
